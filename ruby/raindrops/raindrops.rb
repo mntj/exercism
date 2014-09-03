a0c3a4bb500ced7a099aa25fad62ae1e216c8cd1
mntj <mntj11@gmail.com>
2014-09-02 21:47:30 -0400

    raindrops all tests passing

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,39 @@
+require 'prime'
+class Raindrops
+  def self.convert(num)
+    i = 2
+    quotient = num
+    prime_factors = []
+    return num.to_s if num < 2
+    until prime_factors.inject(:*) === num do
+      if quotient % i === 0 && i.prime?
+        prime_factors << i
+        quotient /= i
+      else
+        i += 1
+      end
+    end
+
+    rain_drops = []
+    prime_factors.each do |x|
+      if x == 3 || x == 5 || x == 7
+        rain_drops << x
+      end
+    end
+    return num.to_s if rain_drops.size == 0
+
+    rain_drops.uniq! if rain_drops.size > 1
+    rain_drops.each_with_index do |x,i|
+      case x
+      when 3
+        rain_drops[i] = 'Pling'
+      when 5
+        rain_drops[i] = 'Plang'
+      when 7
+        rain_drops[i] = 'Plong'
+      end
+    end
+
+    return rain_drops.join
+  end
+end
