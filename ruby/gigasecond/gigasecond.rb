43fe0268b66dbf62cda190dadf1a8d9e71cbf03c
mntj <mntj11@gmail.com>
2014-09-07 01:51:15 -0400

    add gigasecond constant

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -3,8 +3,9 @@ require 'time'
 
 class Gigasecond
   def self.from(birthdate)
+    gigasecond = 10**9
     bday_time = birthdate.to_time
-    gigasecond_anniversary_time = bday_time + 10**9
+    gigasecond_anniversary_time = bday_time + gigasecond
     return gigasecond_anniversary_time.to_date
   end
 end
