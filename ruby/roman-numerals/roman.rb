367e68a0b5d1b9fed3859ce37e48fb66c88c3193
mntj <mntj11@gmail.com>
2014-09-08 00:08:37 -0400

    improved roman_numerals

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,49 +1,31 @@
 class Fixnum
   def to_roman
+    roman_numerals = {
+      1000 => 'M',
+      900  => 'CM',
+      500  => 'D',
+      400  => 'CD',
+      100  => 'C',
+      90   => 'XC',
+      50   => 'L',
+      40   => 'XL',
+      10   => 'X',
+      9    => 'IX',
+      5    => 'V',
+      4    => 'IV',
+      1    => 'I'
+    }
     num = self
     numeral_string = ""
-    while num > 0 do
-      if num >= 1000
-        numeral_string += 'M'
-        num -= 1000
-      elsif num >= 900
-        numeral_string += 'CM'
-        num -= 900
-      elsif num >= 500
-        numeral_string += 'D'
-        num -= 500
-      elsif num >= 400
-        numeral_string += 'CD'
-        num -= 400
-      elsif num >= 100
-        numeral_string += 'C'
-        num -= 100
-      elsif num >= 90
-        numeral_string += 'XC'
-        num -= 90
-      elsif num >= 50
-        numeral_string += 'L'
-        num -= 50
-      elsif num >= 40
-        numeral_string += 'XL'
-        num -= 40
-      elsif num >= 10
-        numeral_string += 'X'
-        num -= 10
-      elsif num >= 9
-        numeral_string += 'IX'
-        num -= 9
-      elsif num >= 5
-        numeral_string += 'V'
-        num -= 5
-      elsif num >= 4
-        numeral_string += 'IV'
-        num -= 4
-      elsif num >= 1
-        numeral_string += 'I'
-        num -= 1
+      while num > 0
+        roman_numerals.each do |int, numeral|
+          if num >= int
+            numeral_string += numeral
+            num -= int
+            break
+          end
+        end
       end
-    end
     return numeral_string
   end
 end
