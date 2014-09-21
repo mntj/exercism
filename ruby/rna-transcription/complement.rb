cde27e600514676e4637e10bb8961a9e1a1ed1da
mntj <mntj11@gmail.com>
2014-09-21 00:22:40 -0400

    further refactoring of rna

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,25 +1,18 @@
 class Complement
-  @da_funk = ->(f,g) { f.chars.map {
+  @complements = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+  $m = ->(f,g) { f.chars.map {
       |x| g[x]
     }.join
   }
   def self.of_dna(dna_string)
-    complements = {
-      'G' => 'C',
-      'C' => 'G',
-      'T' => 'A',
-      'A' => 'U'
-    }
-    @da_funk.call(dna_string, complements)
+    $m.(dna_string, @complements)
   end
-
   def self.of_rna(rna_string)
-    complements = {
-      'C' => 'G',
-      'G' => 'C',
-      'A' => 'T',
-      'U' => 'A'
-    }
-    @da_funk.call(rna_string, complements)
+    $m.(rna_string, @complements.invert)
   end
 end
