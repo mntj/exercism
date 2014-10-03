ab2cd2e0586cdbf5c282094ae6bb8188ebbae25d
mntj <mntj11@gmail.com>
2014-10-02 20:12:43 -0400

    Refactor raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,39 +1,10 @@
 require 'prime'
 class Raindrops
   def self.convert(num)
-    return num.to_s if num < 2
-    i = 2
-    quotient = num
-    prime_factors = []
-
-    until prime_factors.inject(:*) === num do
-      if quotient % i === 0 && i.prime?
-        prime_factors << i
-        quotient /= i
-      else
-        i += 1
-      end
-    end
-
-    rain_drops = []
-    prime_factors.each do |x|
-      if x == 3 || x == 5 || x == 7
-        rain_drops << x
-      end
-    end
-    return num.to_s if rain_drops.size == 0
-
-    rain_drops.uniq! if rain_drops.size > 1
-    rain_drops.each_with_index do |x,i|
-      case x
-      when 3
-        rain_drops[i] = 'Pling'
-      when 5
-        rain_drops[i] = 'Plang'
-      when 7
-        rain_drops[i] = 'Plong'
-      end
-    end
-    rain_drops.join
+    string = ''
+    string += 'Pling' if num % 3 === 0
+    string += 'Plang' if num % 5 === 0
+    string += 'Plong' if num % 7 === 0
+    string.empty? ? num.to_s : string
   end
 end
