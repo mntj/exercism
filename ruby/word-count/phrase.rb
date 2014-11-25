2eb965fd5ae9637f122ae2c92e8d4aa92cefe5db
mntj <mntj11@gmail.com>
2014-11-25 02:44:44 -0500

    Incorporate Aldric's suggestions

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,22 +1,19 @@
 class Phrase
+  attr_reader :raw_str
+
   def initialize(raw_str)
     @raw_str = raw_str
   end
 
-  def get_words
-    @words = @raw_str.gsub(/[^a-zA-Z0-9']/,' ').split.map { |word| word.strip.downcase }
+  def words
+    @words ||= raw_str.gsub(/[^a-zA-Z0-9']/,' ').split.map { |word| word.strip.downcase }
   end
 
   def count_words
-    @words.each_with_object({}) { |word, h| h[word] = @words.count(word) }
-  end
-
-  def get_and_count_words
-    get_words
-    count_words
+    words.each_with_object({}) { |word, h| h[word] = @words.count(word) }
   end
 
   def word_count
-    @word_count ||= get_and_count_words
+    @word_count ||= count_words
   end
 end
