415c4d6a648871eec594c3ed7850e98a2fa01716
mntj <mntj11@gmail.com>
2014-11-28 12:40:55 -0500

    binary all tests passing

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -7,6 +7,9 @@ class Binary
   end
 
   def to_decimal
+    if /[a-z]|[2-9]/ =~ str
+      0
+    end
     arr = str.reverse.chars.map { |x| x.to_i }
     arr.each_with_index.map { |n, i| (2 ** i) * n }.inject(:+)
   end
