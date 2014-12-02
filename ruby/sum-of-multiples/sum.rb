19462fada6a0ec0b77a66d5d21867105e4b73ae8
mntj <mntj11@gmail.com>
2014-12-01 20:53:49 -0500

    sum_of_multiples refactor

--- ruby/sum-of-multiples/sum.rb
+++ ruby/sum-of-multiples/sum.rb
@@ -1,35 +1,20 @@
 class SumOfMultiples
-  def initialize(*base_multiples)
-    @base_multiples = base_multiples
-  end
-
-  def self.base_multiples
-    @base_multiples ||= [3, 5]
-  end
-
-  def base_multiples
-    @base_multiples ||= [3, 5]
-  end
-
-  def self.to(limit)
-    return 0 if limit == 1
-
-    multiples = []
-    base_multiples.each do |bm|
-      (1...limit).each { |n| multiples << n if n % bm == 0 }
-    end
-
-    multiples.uniq.inject(:+)
+  def initialize(*bases)
+    @bases = bases
   end
 
   def to(limit)
     return 0 if limit == 1
 
     multiples = []
-    base_multiples.each do |bm|
+    @bases.each do |bm|
       (1...limit).each { |n| multiples << n if n % bm == 0 }
     end
 
     multiples.uniq.inject(:+)
+  end
+
+  def self.to(limit)
+    SumOfMultiples.new(3, 5).to(limit)
   end
 end
