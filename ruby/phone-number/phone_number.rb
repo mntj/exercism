2dae35b39a9fefdce1f529d3cfbed26a19132ead
Corey Leveen <mntj11@gmail.com>
2015-06-08 00:59:42 -0400

    PhoneNumber functional except for area codes and pretty printing

---
+++ ruby/phone-number/phone_number.rb
@@ -0,0 +1,49 @@
+class PhoneNumber
+  def initialize(str)
+    @str = str
+  end
+
+  attr_reader :str, :nums
+
+  def number
+    return '0000000000' if bad_number?
+    nums[0] = '' if trim_first?
+    nums
+  end
+
+  private
+
+  def bad_number?
+    !length_acceptable? || (length_is_eleven? && !first_is_one?)
+  end
+
+  def length_acceptable?
+    l = raw_nums_length
+    s = str.length
+    s < 15 && l > 9 && l < 12
+  end
+
+  def trim_first?
+    length_is_eleven? && first_is_one?
+  end
+
+  def length_is_eleven?
+    @is_11 ||= raw_nums_length == 11
+  end
+
+  def first_is_one?
+    @is_1 ||= raw_nums[0] == "1"
+  end
+
+  def raw_nums_length
+    @len ||= raw_nums.length
+  end
+
+  def raw_nums
+    @nums ||= str.split(//).select { |n| int_arr.include? n }.join
+  end
+
+  def int_arr
+    (0..9).map(&:to_s)
+  end
+end
