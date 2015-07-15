cd298bed018f9eb89afb637bbbf5b9ad303dc08a
Corey Leveen <mntj11@gmail.com>
2015-07-14 21:26:42 -0400

    Add somewhat helpful descriptors

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,7 +1,7 @@
 class ETL
   def self.transform(old)
-    old.each_with_object({}) do |(k, v), h|
-      v.map { |c| h[c.downcase] = k }
+    old.each_with_object({}) do |(pts, chr_arr), new|
+      chr_arr.map { |chr| new[chr.downcase] = pts }
     end
   end
 end
