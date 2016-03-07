65d9b8bc7e0bc95ccb4f0601c4f6f1ae170feda6
Corey Leveen <mntj11@gmail.com>
2016-03-06 23:22:38 -0500

    Bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,10 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-
+      String.last(input) == "?" -> "Sure."
+      String.strip(input) == "" -> "Fine. Be that way!"
+      String.upcase(input) == input and String.downcase(input) != input -> "Woah, chill out!"
+      true -> "Whatever."
     end
   end
 end
